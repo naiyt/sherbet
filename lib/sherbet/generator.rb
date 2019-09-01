1c7c5852b99811a76ee45ba66c8ae1471d7eedeb
Nate Collings <nate@natecollings.com>
2019-08-31 18:45:37 -0600

    Add a times option and a utilities module
    This utilities module will be nice for demoing
    Sorbet.

--- lib/sherbet/generator.rb
+++ lib/sherbet/generator.rb
@@ -2,12 +2,14 @@ require "set"
 
 module Sherbet
   class Generator
-    def run
-      puts wrapper_emoji * 40
-      puts "\n"
-      puts info.join("\n\n")
-      puts "\n"
-      puts wrapper_emoji * 40
+    def run(number_generated=1)
+      number_generated.times do
+        puts wrapper_emoji * 40
+        puts "\n"
+        puts info.join("\n\n")
+        puts "\n"
+        puts wrapper_emoji * 40
+      end
     end
 
     def info
@@ -16,24 +18,6 @@ module Sherbet
 
     def wrapper_emoji
       "*"
-    end
-
-    def obscure(string, to_change=2)
-      changed = Set.new
-
-      to_change.times do
-        loop do
-          pos = rand(0..string.length - 1)
-          random_char = rand(65..90).chr
-          next if changed.include?(pos) || string[pos] == " " || string[pos].upcase == random_char
-
-          changed.add(pos)
-          string[pos] = /[[:upper:]]/.match(string[pos]) ? random_char.upcase : random_char.downcase
-          break
-        end
-      end
-
-      string
     end
   end
 end
